2866a05702a927d44a87988a7831e329b9d8330e
fanste1998 <fanste1998@hotmail.com>
2019-12-15 14:58:43 +0100

    Крај часа 14.12.2019. JsonObjectRequest смо преправили у StringRequest јер конверзија примљене поруке у JSONObject пада због неправилног исписа сервера (сервер највероватније има инсталирану стару верзију библиотеке за PHP)

--- app/src/main/java/com/rct/rctpolaznik/MainActivity.java
+++ app/src/main/java/com/rct/rctpolaznik/MainActivity.java
@@ -3,16 +3,33 @@ package com.rct.rctpolaznik;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +46,55 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final AppCompatEditText acetMail = findViewById(R.id.content_main_email),
+                acetPassword = findViewById(R.id.content_main_password);
+
+        AppCompatButton acbLogin = findViewById(R.id.content_main_submit);
+        acbLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://fanste1998.000webhostapp.com/user/login.php";
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject jsonResponse = new JSONObject(response);
+                                    if(jsonResponse.getInt("status") == 1) Toast.makeText(MainActivity.this, "Успешно сте се пријавили!", Toast.LENGTH_LONG).show();
+                                    else Toast.makeText(MainActivity.this, "Погрешили сте при уносу!", Toast.LENGTH_LONG).show();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, getErrorMessage(error), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("mail", acetMail.getText().toString());
+                        params.put("password", acetPassword.getText().toString());
+                        return params;
+                    }
+                };
+
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+            }
+        });
+    }
+    private static String getErrorMessage(VolleyError error) {
+        if(error != null) {
+            String err = "Error: " + error.toString();
+            if(error.networkResponse != null) err += " error code:" + error.networkResponse.statusCode;
+            return err;
+        } else return "Unknown error";
     }
 }
