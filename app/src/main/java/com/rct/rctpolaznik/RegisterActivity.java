9507a77f61acf63d3766387cfc49039d24c394be
fanste1998 <fanste1998@hotmail.com>
2020-02-22 18:54:31 +0100

    Restore

---
+++ app/src/main/java/com/rct/rctpolaznik/RegisterActivity.java
@@ -0,0 +1,32 @@
+package com.rct.rctpolaznik;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        AppCompatTextView actvLogin = findViewById(R.id.content_register_login);
+        actvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
